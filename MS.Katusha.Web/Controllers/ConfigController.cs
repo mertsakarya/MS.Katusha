97aa917b5c0407983a666e73d85367f356e9ed1c
Mert Sakarya <mertsakarya@hotmail.com>
2012-08-29 12:26:24 +0300

    config display

---
+++ MS.Katusha.Web/Controllers/ConfigController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using MS.Katusha.Configuration;
+using Newtonsoft.Json;
+
+namespace MS.Katusha.Web.Controllers
+{
+    public class ConfigController : Controller
+    {
+        [HttpGet]
+        public void Display()
+        {
+            var instance = KatushaConfigurationManager.Instance;
+            var settings = instance.GetSettings();
+            var result = new { instance.VirtualPath, instance.ConnectionString, settings.AdministratorMailAddress, settings.Ip, settings.MailViewFolder, settings.NotTrackedIpsByGoogleAnalytics, settings.Protocol };
+            Response.ContentType = "application/json";
+            Response.Write(JsonConvert.SerializeObject(result, Formatting.Indented));
+        }
+    }
+}
