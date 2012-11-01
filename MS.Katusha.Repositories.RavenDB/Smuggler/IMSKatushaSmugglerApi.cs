b5657916e7537e6bc9043b198c6ff851d66e4f43
Mert Sakarya <mertsakarya@hotmail.com>
2012-11-01 10:48:06 +0200

    Yandex for everyone!!!

---
+++ MS.Katusha.Repositories.RavenDB/Smuggler/IMSKatushaSmugglerApi.cs
@@ -0,0 +1,8 @@
+﻿namespace MS.Katusha.Repositories.RavenDB.Smuggler
+{
+    public interface IMSKatushaSmugglerApi
+    {
+        void ExportData(MSKatushaSmugglerOptions options, bool incremental);
+        void ImportData(MSKatushaSmugglerOptions options, bool incremental);
+    }
+}
